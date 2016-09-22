d818210259675bbede2777acba6e7ac3fe51b6ee
Peter Andersson <fshsweden@hotmail.com>
2016-09-22 11:20:19 +0200

    Initial Commit

---
+++ Tracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tracker
+//
+//  Created by Peter Andersson on 2016-09-22.
+//  Copyright © 2016 Peter Andersson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
