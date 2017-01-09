391671da2ea3f710ba3d4dcb679da404ff1b3d38
Peter Andersson <peterandersson@Peters-iMac.lan>
2017-01-09 14:18:38 +0100

    wip

--- Tracker/FirstViewController.swift
+++ Tracker/FirstViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import SwiftPhotoGallery
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, SwiftPhotoGalleryDataSource, SwiftPhotoGalleryDelegate {
+    
+    let imageNames = ["image1.jpeg", "image2.jpeg", "image3.jpeg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        
+        let gallery = SwiftPhotoGallery(delegate: self, dataSource: self)
+        gallery.backgroundColor = UIColor.blackColor()
+        gallery.pageIndicatorTintColor = UIColor.grayColor().colorWithAlphaComponent(0.5)
+        gallery.currentPageIndicatorTintColor = UIColor.whiteColor()
+        
+        presentViewController(gallery, animated: false, completion: { () -> Void in
+            gallery.currentPage = 2
+        })
+        
+    }
+    
+    func numberOfImagesInGallery(gallery: SwiftPhotoGallery) -> Int {
+        return imageNames.count
+    }
+    
+    func imageInGallery(gallery: SwiftPhotoGallery, forIndex: Int) -> UIImage? {
+        return UIImage(named: imageNames[forIndex])
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func galleryDidTapToClose(gallery: SwiftPhotoGallery) {
+        // do something cool like:
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
-
